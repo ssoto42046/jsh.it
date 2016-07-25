7cb4c025ccb8255c9c9ee0208f54ce4c4b23e23e
Hannah Kwon <demouser@MacBook-Air-3.local>
2016-07-25 09:48:40 -0700

    Final main.py upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from google.appengine.api import users
 import webapp2
+#import jinja2
 
 # the signIn class is taken from the 'googleusers' lab we did in class
 # also (going to create) a template to make the pages in the 'if' () and 'else'
@@ -9,14 +10,14 @@ class signIn(webapp2.RequestHandler):
         user = users.get_current_user()
         if user:
             greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                (user.nickname(), users.create_logout_url('/'))) #should this redirect to the main page instead of a separate page that asks the user to sign out?
-        else:
+                (user.nickname(), users.create_logout_url('/mainHandler'))) #this should be our mainHandler page
 
             greeting = ('<a href="%s">Sign in or register</a>.' %
-                users.create_login_url('/'))
+                users.create_login_url('/')) #we just need to edit the CSS so it looks nicer
 
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
-    ('/', signIn)
+    ('/', signIn),
+    #('/mainHandler', mainHandler )
 ], debug=True)
