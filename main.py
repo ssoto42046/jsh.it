ab05a0d3314c83d02d46fe69e229cd6fb5ae8d9a
hsykwon <ksykwon@gmail.com>
2016-07-26 14:58:52 -0700

    added css to signIn page and moved the tweet/pin it buttons

--- main.py
+++ main.py
@@ -18,31 +18,34 @@ class signIn(webapp2.RequestHandler):
             #       (user.nickname(), users.create_logout_url('/')))
 
         template = env.get_template('mainHandler.html')
-        self.response.out.write(template.render())
+        if user:
+            self.response.out.write(template.render())
             #after the user logs in, the website will be redirected to the
             #mainHandler page (which has all the options and etc.) there will
             #also be a 'signout' option which will redirect to the 'signin' page
+        else:
+            template = env.get_template('signIn.html')
+
+            greeting = ('<button class="btnExample" align=center><a href="%s">Sign in</a></button>' % users.create_login_url('/'))
+            self.response.out.write('<html><body>%s</body></html>' % greeting)
+            self.response.out.write(template.render())
 
         # else:
         #     greeting = ('<a href="%s">Sign in or register</a>.' %
         #         users.create_login_url('/')
-            # variables = {
-            #     'logout_url': "users.create_logout_url('/')",
-            # }
+        #     self.response.out.write('<html><body>%s</body></html>' % greeting)
             #we just need to edit the CSS so it looks nicer
-
-        #self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 # the mainHandler class will be the page that allows the user to post photos/statuses
 # and the list of all the social media apps will be displayed on this page as well
 # you will also need to link the facebook/twitter/etc handlers and htmls to this class
+
 class signOutHandler(webapp2.RequestHandler):
     def get(self):
-        user = users.get_current_user()
-        greeting = ('<a href="%s">Sign in or register</a>.' %
-            users.create_login_url('/'))
+        signouturl = users.create_logout_url('/')
+        self.redirect(signouturl)
+        #self.response.out.write('<html><body>%s</body></html>' % greeting)
 
-        self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
     ('/', signIn),
