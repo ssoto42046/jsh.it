c352c9b51cd15262843301b8f0901b8818e99d0e
hsykwon <ksykwon@gmail.com>
2016-07-25 22:44:46 -0700

    Updated main.py, added pictures, and tweet button

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from google.appengine.api import users
 import webapp2
-#import jinja2
+import jinja2
+
+#this is the line of code that allows for you to connect the main.py with the templates
+
+env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
+
 
 # the signIn class is taken from the 'googleusers' lab we did in class
 # also (going to create) a template to make the pages in the 'if' () and 'else'
@@ -8,16 +13,40 @@ import webapp2
 class signIn(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
-        if user:
-            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
-                (user.nickname(), users.create_logout_url('/mainHandler'))) #this should be our mainHandler page
+        # if user:
+            # greeting = ('Get ready to post.it, %s! (<a href="%s">sign out</a>)' %
+            #       (user.nickname(), users.create_logout_url('/')))
 
-            greeting = ('<a href="%s">Sign in or register</a>.' %
-                users.create_login_url('/')) #we just need to edit the CSS so it looks nicer
+        template = env.get_template('mainHandler.html')
+        self.response.out.write(template.render())
+            #after the user logs in, the website will be redirected to the
+            #mainHandler page (which has all the options and etc.) there will
+            #also be a 'signout' option which will redirect to the 'signin' page
+
+        # else:
+        #     greeting = ('<a href="%s">Sign in or register</a>.' %
+        #         users.create_login_url('/')
+            # variables = {
+            #     'logout_url': "users.create_logout_url('/')",
+            # }
+            #we just need to edit the CSS so it looks nicer
+
+        #self.response.out.write('<html><body>%s</body></html>' % greeting)
+
+# the mainHandler class will be the page that allows the user to post photos/statuses
+# and the list of all the social media apps will be displayed on this page as well
+# you will also need to link the facebook/twitter/etc handlers and htmls to this class
+class signOutHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        greeting = ('<a href="%s">Sign in or register</a>.' %
+            users.create_login_url('/'))
 
         self.response.out.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
     ('/', signIn),
-    #('/mainHandler', mainHandler )
+    #('/mainHandler', mainHandler),
+    ('/signOutHandler', signOutHandler)
+
 ], debug=True)
---
+++ static/mainHandler.js
@@ -0,0 +1,11 @@
+// $("button").click(signOut);
+//
+// function signOut() {
+//   location.replace("http://www.w3schools.com");
+// }
+//
+// $(document).ready(function(){
+//     $("button").click(function(){
+//         location.replace({{logout_url}});
+//     });
+// });
